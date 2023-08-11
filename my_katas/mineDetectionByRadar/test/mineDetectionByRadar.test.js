e6047084c2ce6e9c68c3feab905af9aa2c3ee800
daniel.mesu <daniel.mesu@coderio.co>
2023-08-11 20:48:10 +0100

    added basic and random tests

--- my_katas/mineDetectionByRadar/test/mineDetectionByRadar.test.js
+++ my_katas/mineDetectionByRadar/test/mineDetectionByRadar.test.js
@@ -4,8 +4,34 @@ const assert = chai.assert;
 
 const { mineDetectionByRadar } = require("../mineDetectionByRadar");
 
+function randomDataGenerator(){
+  let data = [];
+
+  for(let x = 0; x < 100; x++){
+      const arrayLength = Math.floor(Math.random() * 20)
+      const testData = Array.from({length: arrayLength}).map(() => []);
+
+      const randomInput = testData.map(array => {
+          const vibration = Math.floor(Math.random() * (5 - 1 + 1)) + 1;
+          const anomalies = ["0","(","X","#","*"];
+          const anomalyIndex = Math.floor(Math.random() * anomalies.length);
+
+          array.push(vibration);
+          array.push(anomalies[anomalyIndex]);
+
+          return array;
+      });
+
+      data.push(randomInput);
+  }
+
+  return data;
+}
+
 describe('Using assert', () => {
+
   it('Basic Tests', () => {
+
     assert.deepEqual(
       mineDetectionByRadar([[2,"X"],[4,"X"],[5,"("],[2,"*"],[1,"*"]]), 
       [ 1, 2, 3 ]
@@ -40,6 +66,19 @@ describe('Using assert', () => {
       mineDetectionByRadar([[3, "#"], [4, "X"], [5, "#"], [1, "0"], [2, "X"], [3, "0"], [4, "*"], [5, "#"], [1, "("], [2, "X"], [3, "#"], [4, "*"], [5, "("], [1, "0"], [2, "X"], [3, "#"]]), 
       [ 0, 1, 2, 6, 7, 10, 11, 12, 15]
     );
+
+  });
+
+  it('Random Tests', async() => {
+    const randomData = randomDataGenerator();
+    const randomResults = randomData.map(data => mineDetectionByRadar(data));
+
+    await randomData.forEach((data,index) => {
+      assert.deepEqual(
+        mineDetectionByRadar(data), 
+        randomResults[index]
+      );
+    });
 
   });
 });
